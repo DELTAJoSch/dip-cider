cc77654a67fc90b8ec35b6030d5c0ef14ccdb8cf
DELTAJoSch <deltajosch@gmail.com>
2019-09-02 14:57:10 +0200

    Acceleration Timed working

--- CIDER/CIDER/ViewModels/AccelerationGraphViewModel.cs
+++ CIDER/CIDER/ViewModels/AccelerationGraphViewModel.cs
@@ -21,23 +21,22 @@ namespace CIDER.ViewModels
         {
             _data = Data;
 
+            LoadPlot();
+        }
+
+        private async void LoadPlot()
+        ///This code cannot be executed directly from the constructor as this is async
+        ///Due to it being void, this code is executed "Fire and Forget"
+        {
             //Create a new PlotModel, associate the corresponding DataPoints and display it
             PlotModel CreatePlot = new PlotModel();
             CreatePlot.Title = "Acceleration";
 
+            var Task = GetLineSeriesAsync();
+
             LineSeries XSeries = new LineSeries();
             LineSeries YSeries = new LineSeries();
             LineSeries ZSeries = new LineSeries();
-
-            double t = 0;
-
-            foreach (Tuple<float, float, float> tuple in _data.Acceleration)
-            {
-                XSeries.Points.Add(new DataPoint(t, (double)tuple.Item1));
-                YSeries.Points.Add(new DataPoint(t, (double)tuple.Item2));
-                ZSeries.Points.Add(new DataPoint(t, (double)tuple.Item3));
-                t++;
-            }
 
             XSeries.Color = OxyColor.FromRgb(38, 255, 0);
             YSeries.Color = OxyColor.FromRgb(255, 0, 230);
@@ -47,6 +46,14 @@ namespace CIDER.ViewModels
             YSeries.Title = "L/R";
             ZSeries.Title = "U/D";
 
+            await Task;
+
+            var res = Task.Result;
+
+            XSeries.Points.AddRange(res.Item1);
+            YSeries.Points.AddRange(res.Item2);
+            ZSeries.Points.AddRange(res.Item3);
+
             CreatePlot.Series.Add(XSeries);
             CreatePlot.Series.Add(YSeries);
             CreatePlot.Series.Add(ZSeries);
@@ -54,6 +61,27 @@ namespace CIDER.ViewModels
             Plot = CreatePlot;
 
             Plot.InvalidatePlot(true);
+        }
+
+        private async Task<Tuple<List<DataPoint>, List<DataPoint>, List<DataPoint>>> GetLineSeriesAsync()
+        ///This Task reads all the data points and convert them to line series
+        ///It´s executed asynchronously, so this doesn't block when large datasets are involved
+        {
+            List<DataPoint> XData = new List<DataPoint>();
+            List<DataPoint> YData = new List<DataPoint>();
+            List<DataPoint> ZData = new List<DataPoint>();
+
+            double t = 0;
+
+            foreach (Tuple<float, float, float> tuple in _data.Acceleration)
+            {
+                XData.Add(new DataPoint(t, (double)tuple.Item1));
+                YData.Add(new DataPoint(t, (double)tuple.Item2));
+                ZData.Add(new DataPoint(t, (double)tuple.Item3));
+                t++;
+            }
+
+            return new Tuple<List<DataPoint>, List<DataPoint>, List<DataPoint>>(XData,YData,ZData);
         }
 
         public PlotModel Plot
--- CIDER/CIDER/Views/AccelerationTimed.xaml.cs
+++ CIDER/CIDER/Views/AccelerationTimed.xaml.cs
@@ -21,13 +21,19 @@ namespace CIDER.Views
     /// </summary>
     public partial class AccelerationTimed : Page
     {
+        private AccelerationTimedViewModel model;
         public AccelerationTimed(DataProvider data)
         {
             InitializeComponent();
 
-            AccelerationTimedViewModel model = new AccelerationTimedViewModel(data);
+            model = new AccelerationTimedViewModel(data);
 
             this.DataContext = model;
+        }
+
+        private void slValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            model.SliderValueChanged((int)slValue.Value);
         }
     }
 }
